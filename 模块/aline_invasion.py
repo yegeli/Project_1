95112f021afda078bf0c408e69efbd47a333b26b
1332812330@qq.com <1332812330@qq.com>
2019-11-14 11:22:43 +0800

    左右移动设置边界，重构check_events()函数

--- 模块/aline_invasion.py
+++ 模块/aline_invasion.py
@@ -1,3 +1,4 @@
+# aline_inbasion.py
 import pygame
 from settings import Settings
 from ship import Ship
@@ -9,11 +10,13 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("小子，你瞅啥啊")
     '''创建一艘飞船'''
-    ship = Ship(screen)
+    ship = Ship(ai_settings,screen)
     '''开始游戏的主循环'''
     while True:
         '''监视键盘和鼠标事件'''
-        gf.check_events()
+        gf.check_events(ship)
+        # 根据移动标志调整飞船的位置
+        ship.update()
         '''每次循环时都重绘屏幕'''
         gf.update_screen(ai_settings,screen,ship)
 
--- 模块/settings.py
+++ 模块/settings.py
@@ -1,8 +1,11 @@
+# settings.py
 class Settings():
     '''存储《外星人入侵》的所有设置的类'''
     def __init__(self):
         '''初始化游戏设置'''
-
         self.screen_width = 1200  # 屏幕设置尺寸
         self.screen_height = 800
         self.bg_color = (230,230,230) # 屏幕背景色
+        # 飞船的设置
+        self.ship_speed_factor = 1.5
+
